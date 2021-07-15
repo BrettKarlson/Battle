08bdf1df9a920afd28ddfa0e6cb17d1c0fb033d5
Brett Karlson <brettjkarlson@gmail.com>
2021-07-15 21:44:06 +0100

    two player functionality implemented

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -6,11 +6,29 @@ feature 'Attacking' do
     expect(page).to have_content 'Matthew attacked Richard'
   end
 
+  scenario 'be attacked by Player 2' do
+    sign_in_and_play
+    click_button 'Attack'
+    click_button 'OK'
+    click_button 'Attack'
+    expect(page).to have_content 'Richard attacked Matthew'
+  end
+
   scenario 'reduce Player 2 HP by 10' do
     sign_in_and_play
     click_button 'Attack'
-    click_link 'OK'
+    click_button 'OK'
     expect(page).not_to have_content 'Richard: 60HP'
     expect(page).to have_content 'Richard: 50HP'
+  end
+
+  scenario 'reduce Player 1 HP by 10' do
+    sign_in_and_play
+    click_button 'Attack'
+    click_button 'OK'
+    click_button 'Attack'
+    click_button 'OK'
+    expect(page).not_to have_content 'Matthew: 60HP'
+    expect(page).to have_content 'Matthew: 50HP'
   end
 end
--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -4,5 +4,10 @@ feature 'View hit points' do
     sign_in_and_play
     expect(page).to have_content 'Richard: 60HP'
   end
+
+  scenario 'see Player 1 hit points' do
+    sign_in_and_play
+    expect(page).to have_content 'Matthew: 60HP'
+  end
 end
 
