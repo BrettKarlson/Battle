e9a92f00e4ab7f6af55aea9e0eaa9be6f801c90a
Brett Karlson <brettjkarlson@gmail.com>
2021-07-15 13:38:10 +0100

    two user stories complete

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,13 +1,8 @@
 
 feature 'Enter names' do
   scenario 'submitting names' do
-    visit('/')
-    fill_in :player_1_name, with: 'Matthew'
-    fill_in :player_2_name, with: 'Richard'
-    click_button 'Submit'
-
-    save_and_open_page
-
-    expect(page).to have_content 'Matthew vs. Richard'
+    sign_in_and_play
+     #save_and_open_page
+     expect(page).to have_content 'Matthew vs. Richard'
   end
 end
---
+++ spec/features/web_helpers.rb
@@ -0,0 +1,8 @@
+# in web_helpers.rb
+
+def sign_in_and_play
+  visit('/')
+  fill_in :player_1_name, with: 'Matthew'
+  fill_in :player_2_name, with: 'Richard'
+  click_button 'Submit'
+end
