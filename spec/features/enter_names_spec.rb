4e5576ed0ba86a304e4b907c025ce09353332a89
Brett Karlson <brettjkarlson@gmail.com>
2021-07-15 13:55:24 +0100

    user story 3 finished

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -2,7 +2,6 @@
 feature 'Enter names' do
   scenario 'submitting names' do
     sign_in_and_play
-     #save_and_open_page
-     expect(page).to have_content 'Matthew vs. Richard'
+    expect(page).to have_content 'Matthew vs. Richard'
   end
 end
